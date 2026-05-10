5d37ae883e0c142e2c779ff522fb44f4ca0f2151
liyongshang44-max <liyongshang44@gmail.com>
2026-05-10 23:12:42 +0800

    feat(flight-table): register FT-E run control routes

--- apps/server/src/modules/devtools/registerDevtoolsModule.ts
+++ apps/server/src/modules/devtools/registerDevtoolsModule.ts
@@ -5,10 +5,12 @@ import { registerSimConfigRoutes } from "../../routes/sim_config.js";
 import { registerDeviceSimulatorV1Routes } from "../../routes/device_simulator_v1.js";
 import { registerFlightTableV1Routes } from "../../routes/dev/flight_table_v1.js";
 import { registerFlightTableSkillRoutesV1 } from "../../routes/dev/flight_table_skills_v1.js";
+import { registerFlightTableRunControlRoutesV1 } from "../../routes/dev/flight_table_run_control_v1.js";
 
 export function registerDevtoolsModule(app: FastifyInstance, pool: Pool): void {
   registerSimConfigRoutes(app);
   registerDeviceSimulatorV1Routes(app, pool);
   registerFlightTableV1Routes(app, pool);
   registerFlightTableSkillRoutesV1(app, pool);
+  registerFlightTableRunControlRoutesV1(app);
 }
