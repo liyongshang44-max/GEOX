3fa0592bfd99723d35a87cb4fe988047df5b8e3e
liyongshang44-max <liyongshang44@gmail.com>
2026-05-12 17:04:02 +0800

    fix(customer): register customer scope response hook

--- apps/server/src/modules/reporting/registerReportingModule.ts
+++ apps/server/src/modules/reporting/registerReportingModule.ts
@@ -8,10 +8,12 @@ import { registerDashboardV1Routes } from "../../routes/dashboard_v1.js";
 import { registerCustomerV1Routes } from "../../routes/customer_v1.js";
 import { registerOperationReportChainHookV1 } from "../../routes/operation_report_chain_hook_v1.js";
 import { registerFieldReportSemanticsHookV1 } from "../../routes/field_report_semantics_hook_v1.js";
+import { registerCustomerScopeResponseHookV1 } from "../../routes/customer_scope_response_hook_v1.js";
 
 export function registerReportingModule(app: FastifyInstance, pool: Pool): void {
   registerOperationReportChainHookV1(app, pool);
   registerFieldReportSemanticsHookV1(app, pool);
+  registerCustomerScopeResponseHookV1(app);
   registerOperationStateV1Routes(app, pool);
   registerReportsV1Routes(app, pool);
   registerReportsDashboardV1Routes(app, pool);
