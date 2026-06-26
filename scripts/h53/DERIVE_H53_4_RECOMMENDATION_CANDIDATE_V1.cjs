58899c7fa5ef579a3789ce70fcf56b84fb7b56af
liyongshang44-max <liyongshang44@gmail.com>
2026-06-26 12:57:52 +0800

    H53.4 point entrypoint at runner

--- scripts/h53/DERIVE_H53_4_RECOMMENDATION_CANDIDATE_V1.cjs
+++ scripts/h53/DERIVE_H53_4_RECOMMENDATION_CANDIDATE_V1.cjs
@@ -3,7 +3,7 @@
 // scripts/h53/DERIVE_H53_4_RECOMMENDATION_CANDIDATE_V1.cjs
 // Purpose: entrypoint for H53.4 recommendation candidate derivation.
 
-const { main } = require('./h53_4_recommendation_candidate_main.cjs');
+const { main } = require('./h53_4_runner.cjs');
 
 main().catch((error) => {
   console.error(JSON.stringify({ ok: false, derivation: 'DERIVE_H53_4_RECOMMENDATION_CANDIDATE_V1', error: error.message }, null, 2));
