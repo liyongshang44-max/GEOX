d55e0bcfebecf80b9dea60f90d5e7fac0e035bad
liyongshang44-max <liyongshang44@gmail.com>
2026-06-29 01:59:19 +0800

    Remove POSTV1-02 placeholder acceptance

--- scripts/governance_acceptance/POSTV1_02_STRONG_MULTI_SCOPE_FIXTURE_PACK.cjs
+++
@@ -1,5 +0,0 @@
-// scripts/governance_acceptance/POSTV1_02_STRONG_MULTI_SCOPE_FIXTURE_PACK.cjs
-'use strict';
-
-console.log(JSON.stringify({ ok: false, acceptance: 'POSTV1_02_STRONG_MULTI_SCOPE_FIXTURE_PACK', error: 'PLACEHOLDER_NOT_ACCEPTANCE_READY' }, null, 2));
-process.exit(1);
