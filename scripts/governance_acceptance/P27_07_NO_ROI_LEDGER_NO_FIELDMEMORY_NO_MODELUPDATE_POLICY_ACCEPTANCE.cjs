02b52d40598e47f9a4ba2a09e4cf07758bd06a9c
liyongshang44-max <liyongshang44@gmail.com>
2026-07-01 21:36:19 +0800

    P27 wire acceptance entrypoint

--- scripts/governance_acceptance/P27_07_NO_ROI_LEDGER_NO_FIELDMEMORY_NO_MODELUPDATE_POLICY_ACCEPTANCE.cjs
+++ scripts/governance_acceptance/P27_07_NO_ROI_LEDGER_NO_FIELDMEMORY_NO_MODELUPDATE_POLICY_ACCEPTANCE.cjs
@@ -1,1 +1,1 @@
-console.log('{}')
+const h=require('./P27_ACCEPTANCE_HELPERS.cjs');const n=['P27','07','NO','ROI','LEDGER','NO','FIELDMEMORY','NO','MODELUPDATE','POLICY','ACCEPTANCE'].join('_');h.run(n);
