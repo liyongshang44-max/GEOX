a72340e575150be3eb0a722617c0decdcb60502b
liyongshang44-max <liyongshang44@gmail.com>
2026-07-01 17:01:31 +0800

    P25 add acceptance entrypoints

---
+++ scripts/governance_acceptance/P25_00_POST_P24_BASELINE_FREEZE_INHERITANCE_AUDIT.cjs
@@ -0,0 +1,2 @@
+// scripts/governance_acceptance/P25_00_POST_P24_BASELINE_FREEZE_INHERITANCE_AUDIT.cjs
+'use strict';const path=require('node:path');require('./P25_ACCEPTANCE_HELPERS.cjs').run(path.basename(__filename,'.cjs'));
