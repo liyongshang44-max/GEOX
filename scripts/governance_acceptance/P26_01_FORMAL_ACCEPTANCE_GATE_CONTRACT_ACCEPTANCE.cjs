e8b26ae687362ad0a2c09d663a910c2a30698a75
liyongshang44-max <liyongshang44@gmail.com>
2026-07-01 17:56:21 +0800

    P26 add acceptance entrypoints

---
+++ scripts/governance_acceptance/P26_01_FORMAL_ACCEPTANCE_GATE_CONTRACT_ACCEPTANCE.cjs
@@ -0,0 +1,2 @@
+// scripts/governance_acceptance/P26_01_FORMAL_ACCEPTANCE_GATE_CONTRACT_ACCEPTANCE.cjs
+'use strict';const path=require('node:path');require('./P26_ACCEPTANCE_HELPERS.cjs').run(path.basename(__filename,'.cjs'));
