7a3ccb2840c6b393281d1b889f6b4170a8cbd096
liyongshang44-max <liyongshang44@gmail.com>
2026-07-01 17:02:34 +0800

    P25 add acceptance entrypoints

---
+++ scripts/governance_acceptance/P25_03_CONTROLLED_RECEIPT_INTAKE_AUTHORIZATION_CONTRACT_ACCEPTANCE.cjs
@@ -0,0 +1,2 @@
+// scripts/governance_acceptance/P25_03_CONTROLLED_RECEIPT_INTAKE_AUTHORIZATION_CONTRACT_ACCEPTANCE.cjs
+'use strict';const path=require('node:path');require('./P25_ACCEPTANCE_HELPERS.cjs').run(path.basename(__filename,'.cjs'));
