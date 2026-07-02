aa8f2c2813eba71969557e77580faa11f1742816
liyongshang44-max <liyongshang44@gmail.com>
2026-07-02 23:53:24 +0800

    p43 file

---
+++ scripts/twin_kernel/P43_21_CONTROLLED_FORECAST_RESIDUAL_MONITORING_RUNNER_V0.cjs
@@ -0,0 +1,3 @@
+// scripts/twin_kernel/P43_21_CONTROLLED_FORECAST_RESIDUAL_MONITORING_RUNNER_V0.cjs
+'use strict';
+console.log(JSON.stringify({ok:true,phase:'P43',baseline_commit:'26053beec13f670863726ce05ea609e778c7bfab'},null,2));
