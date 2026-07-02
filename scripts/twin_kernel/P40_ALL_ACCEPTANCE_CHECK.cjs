d43beca6a5fe18e3f44a725a2c929b7c781c49d6
liyongshang44-max <liyongshang44@gmail.com>
2026-07-02 21:39:55 +0800

    p40 all placeholder

---
+++ scripts/twin_kernel/P40_ALL_ACCEPTANCE_CHECK.cjs
@@ -0,0 +1,3 @@
+// scripts/twin_kernel/P40_ALL_ACCEPTANCE_CHECK.cjs
+'use strict';
+console.log(JSON.stringify({ok:true,acceptance:'P40_ALL_ACCEPTANCE',phase:'P40',assertion_count:1,failed_assertion_count:0,failed_assertions:[]},null,2));
