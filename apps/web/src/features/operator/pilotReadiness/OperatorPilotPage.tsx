13732ce9eca7e8eacb9c9dd3e5305db55a9b51f1
mylr <liyongshang44@gmail.com>
2026-07-05 00:29:03 +0800

    fix: complete H63 pilot readiness product surface

--- apps/web/src/features/operator/pilotReadiness/OperatorPilotPage.tsx
+++ apps/web/src/features/operator/pilotReadiness/OperatorPilotPage.tsx
@@ -3,6 +3,7 @@
 
 import React from "react";
 import { buildPilotReadinessViewModel, type PilotReadinessRow } from "./pilotReadinessViewModel";
+import "../../../styles/operatorPilotReadiness.css";
 
 type TablePanelProps = {
   title: string;
