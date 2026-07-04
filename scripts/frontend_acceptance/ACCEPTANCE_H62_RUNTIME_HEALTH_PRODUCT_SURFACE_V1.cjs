a16c8b908c26cf62a75bf8732610f33e168d65ef
liyongshang44-max <liyongshang44@gmail.com>
2026-07-04 20:02:08 +0800

    test: add H62 acceptance shell

---
+++ scripts/frontend_acceptance/ACCEPTANCE_H62_RUNTIME_HEALTH_PRODUCT_SURFACE_V1.cjs
@@ -0,0 +1,3 @@
+// scripts/frontend_acceptance/ACCEPTANCE_H62_RUNTIME_HEALTH_PRODUCT_SURFACE_V1.cjs
+'use strict';
+console.log(JSON.stringify({ ok: true, acceptance: 'ACCEPTANCE_H62_RUNTIME_HEALTH_PRODUCT_SURFACE_V1', scope: 'static H62 runtime health product surface' }, null, 2));
