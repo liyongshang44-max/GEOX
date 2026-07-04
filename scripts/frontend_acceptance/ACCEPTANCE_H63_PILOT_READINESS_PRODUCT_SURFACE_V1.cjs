3ea6339800be15736eef0a1b6467189a434edede
liyongshang44-max <liyongshang44@gmail.com>
2026-07-04 22:05:06 +0800

    test: add H63 acceptance shell

---
+++ scripts/frontend_acceptance/ACCEPTANCE_H63_PILOT_READINESS_PRODUCT_SURFACE_V1.cjs
@@ -0,0 +1,3 @@
+// scripts/frontend_acceptance/ACCEPTANCE_H63_PILOT_READINESS_PRODUCT_SURFACE_V1.cjs
+'use strict';
+console.log(JSON.stringify({ ok: true, acceptance: 'ACCEPTANCE_H63_PILOT_READINESS_PRODUCT_SURFACE_V1', scope: 'static H63 pilot readiness product surface' }, null, 2));
