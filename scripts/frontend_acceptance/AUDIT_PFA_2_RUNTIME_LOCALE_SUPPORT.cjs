54b1962d59454f4d009608e9739b1081457184d6
liyongshang44-max <liyongshang44@gmail.com>
2026-07-08 01:57:57 +0800

    refactor(pfa): route locale audit through neutral-aware support

--- scripts/frontend_acceptance/AUDIT_PFA_2_RUNTIME_LOCALE_SUPPORT.cjs
+++ scripts/frontend_acceptance/AUDIT_PFA_2_RUNTIME_LOCALE_SUPPORT.cjs
@@ -1,4 +1,4 @@
 // scripts/frontend_acceptance/AUDIT_PFA_2_RUNTIME_LOCALE_SUPPORT.cjs
 'use strict';
 
-module.exports = require('./AUDIT_PFA_2_RUNTIME_LOCALE_SUPPORT_V2.cjs');
+module.exports = require('./AUDIT_PFA_2_RUNTIME_LOCALE_SUPPORT_V3.cjs');
