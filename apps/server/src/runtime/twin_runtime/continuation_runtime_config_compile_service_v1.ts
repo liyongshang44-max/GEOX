d8e377111df6f4bcc5afbd645851a006edc68606
liyongshang44-max <liyongshang44@gmail.com>
2026-07-10 03:51:00 +0800

    MCFT-CAP-02: freeze continuation runtime config compiler inputs

--- apps/server/src/runtime/twin_runtime/continuation_runtime_config_compile_service_v1.ts
+++ apps/server/src/runtime/twin_runtime/continuation_runtime_config_compile_service_v1.ts
@@ -10,12 +10,21 @@ import {
 import {
   MCFT_CAP_02_CONTINUATION_CONFIG_PURPOSE_V1,
   MCFT_CAP_02_CONTINUATION_CONFIG_SELECTION_MODE_V1,
+  MCFT_CAP_02_CONTINUATION_CROP_ROOT_ZONE_DEPTH_MM_V1,
+  MCFT_CAP_02_CONTINUATION_CROP_STAGE_CONTEXT_HASH_V1,
   MCFT_CAP_02_CONTINUATION_CROP_STAGE_CONTEXT_KIND_V1,
+  MCFT_CAP_02_CONTINUATION_CROP_STAGE_CONTEXT_REF_V1,
+  MCFT_CAP_02_CONTINUATION_CROP_ET_RELATIVE_STDDEV_V1,
   MCFT_CAP_02_CONTINUATION_DRAINAGE_COEFFICIENT_PER_HOUR_V1,
+  MCFT_CAP_02_CONTINUATION_DYNAMICS_MODEL_COMPONENT_REF_V1,
+  MCFT_CAP_02_CONTINUATION_EXECUTED_IRRIGATION_RELATIVE_STDDEV_V1,
   MCFT_CAP_02_CONTINUATION_FIELD_CAPACITY_FRACTION_V1,
   MCFT_CAP_02_CONTINUATION_FIELD_CAPACITY_STORAGE_MM_V1,
+  MCFT_CAP_02_CONTINUATION_FORECAST_BLOCK_POLICY_COMPONENT_REF_V1,
   MCFT_CAP_02_CONTINUATION_FORECAST_BLOCK_POLICY_V1,
   MCFT_CAP_02_CONTINUATION_MODEL_ID_V1,
+  MCFT_CAP_02_CONTINUATION_MODEL_COMPONENT_REFS_V1,
+  MCFT_CAP_02_CONTINUATION_NO_OBSERVATION_POLICY_COMPONENT_REF_V1,
   MCFT_CAP_02_CONTINUATION_NO_OBSERVATION_POLICY_V1,
   MCFT_CAP_02_CONTINUATION_OUTPUT_DECIMALS_V1,
   MCFT_CAP_02_CONTINUATION_PROCESS_UNCERTAINTY_POLICY_V1,
@@ -25,11 +34,12 @@ import {
   MCFT_CAP_02_CONTINUATION_RUNOFF_FRACTION_V1,
   MCFT_CAP_02_CONTINUATION_SATURATION_FRACTION_V1,
   MCFT_CAP_02_CONTINUATION_SATURATION_STORAGE_MM_V1,
+  MCFT_CAP_02_CONTINUATION_SOIL_HYDRAULIC_SOURCE_CONFIG_HASH_V1,
+  MCFT_CAP_02_CONTINUATION_SOIL_HYDRAULIC_SOURCE_CONFIG_REF_V1,
+  MCFT_CAP_02_CONTINUATION_SOIL_ROOT_ZONE_CONFIG_REF_V1,
+  MCFT_CAP_02_CONTINUATION_SPATIAL_OVERLAP_POLICY_V1,
   MCFT_CAP_02_CONTINUATION_STRUCTURAL_PROCESS_STDDEV_MM_PER_HOUR_V1,
-  MCFT_CAP_02_CONTINUATION_CROP_ET_RELATIVE_STDDEV_V1,
-  MCFT_CAP_02_CONTINUATION_EXECUTED_IRRIGATION_RELATIVE_STDDEV_V1,
-  MCFT_CAP_02_CONTINUATION_COVARIANCE_POLICY_V1,
-  MCFT_CAP_02_CONTINUATION_CROP_ROOT_ZONE_DEPTH_MM_V1,
+  MCFT_CAP_02_CONTINUATION_UNCERTAINTY_POLICY_COMPONENT_REF_V1,
   MCFT_CAP_02_CONTINUATION_WILTING_POINT_FRACTION_V1,
   MCFT_CAP_02_CONTINUATION_WILTING_POINT_STORAGE_MM_V1,
   MCFT_CAP_02_CONTINUATION_COMPUTATION_STORAGE_VARIANCE_SCALE_V1,
@@ -68,6 +78,22 @@ function requireArrayV1(value: unknown, code: string): string[] {
 }
 
 function freezePayloadV1(input: CompileContinuationRuntimeConfigInputV1): ContinuationRuntimeConfigSemanticPayloadV1 {
+  const cropStageContextRef = requireStringV1(input.crop_stage_context_ref, "CROP_STAGE_CONTEXT_REF_REQUIRED");
+  const cropStageContextHash = requireStringV1(input.crop_stage_context_hash, "CROP_STAGE_CONTEXT_HASH_REQUIRED");
+  const soilHydraulicSourceConfigRef = requireStringV1(input.soil_hydraulic_source_config_ref, "SOIL_HYDRAULIC_SOURCE_CONFIG_REF_REQUIRED");
+  const soilHydraulicSourceConfigHash = requireStringV1(input.soil_hydraulic_source_config_hash, "SOIL_HYDRAULIC_SOURCE_CONFIG_HASH_REQUIRED");
+  const dynamicsModelComponentRef = requireStringV1(input.dynamics_model_component_ref, "DYNAMICS_MODEL_COMPONENT_REF_REQUIRED");
+  const soilRootZoneConfigRef = requireStringV1(input.soil_root_zone_config_ref, "SOIL_ROOT_ZONE_CONFIG_REF_REQUIRED");
+  const modelComponentRefs = requireArrayV1(input.model_component_refs, "MODEL_COMPONENT_REFS_REQUIRED");
+
+  if (cropStageContextRef !== MCFT_CAP_02_CONTINUATION_CROP_STAGE_CONTEXT_REF_V1) throw new Error("CROP_STAGE_CONTEXT_REF_MISMATCH");
+  if (cropStageContextHash !== MCFT_CAP_02_CONTINUATION_CROP_STAGE_CONTEXT_HASH_V1) throw new Error("CROP_STAGE_CONTEXT_HASH_MISMATCH");
+  if (soilHydraulicSourceConfigRef !== MCFT_CAP_02_CONTINUATION_SOIL_HYDRAULIC_SOURCE_CONFIG_REF_V1) throw new Error("SOIL_HYDRAULIC_SOURCE_CONFIG_REF_MISMATCH");
+  if (soilHydraulicSourceConfigHash !== MCFT_CAP_02_CONTINUATION_SOIL_HYDRAULIC_SOURCE_CONFIG_HASH_V1) throw new Error("SOIL_HYDRAULIC_SOURCE_CONFIG_HASH_MISMATCH");
+  if (dynamicsModelComponentRef !== MCFT_CAP_02_CONTINUATION_DYNAMICS_MODEL_COMPONENT_REF_V1) throw new Error("DYNAMICS_MODEL_COMPONENT_REF_MISMATCH");
+  if (soilRootZoneConfigRef !== MCFT_CAP_02_CONTINUATION_SOIL_ROOT_ZONE_CONFIG_REF_V1) throw new Error("SOIL_ROOT_ZONE_CONFIG_REF_MISMATCH");
+  if (JSON.stringify(modelComponentRefs) !== JSON.stringify([...MCFT_CAP_02_CONTINUATION_MODEL_COMPONENT_REFS_V1])) throw new Error("MODEL_COMPONENT_REFS_MISMATCH");
+
   return {
     config_purpose: MCFT_CAP_02_CONTINUATION_CONFIG_PURPOSE_V1,
     config_selection_mode: MCFT_CAP_02_CONTINUATION_CONFIG_SELECTION_MODE_V1,
@@ -80,19 +106,19 @@ function freezePayloadV1(input: CompileContinuationRuntimeConfigInputV1): Contin
     geometry_semantic_hash: requireStringV1(input.geometry_semantic_hash, "GEOMETRY_SEMANTIC_HASH_REQUIRED"),
     crop_stage_context: {
       context_kind: MCFT_CAP_02_CONTINUATION_CROP_STAGE_CONTEXT_KIND_V1,
-      context_ref: requireStringV1(input.crop_stage_context_ref, "CROP_STAGE_CONTEXT_REF_REQUIRED"),
-      context_hash: requireStringV1(input.crop_stage_context_hash, "CROP_STAGE_CONTEXT_HASH_REQUIRED"),
+      context_ref: cropStageContextRef,
+      context_hash: cropStageContextHash,
       resolution_policy_id: MCFT_CAP_02_CONTINUATION_ROOT_ZONE_POLICY_ID_V1,
     },
     dynamics_model: {
-      model_component_ref: requireStringV1(input.dynamics_model_component_ref, "DYNAMICS_MODEL_COMPONENT_REF_REQUIRED"),
+      model_component_ref: dynamicsModelComponentRef,
       model_id: MCFT_CAP_02_CONTINUATION_MODEL_ID_V1,
       model_version: 1,
       step_duration: "PT1H",
     },
     soil_hydraulic_snapshot: {
-      source_config_ref: requireStringV1(input.soil_hydraulic_source_config_ref, "SOIL_HYDRAULIC_SOURCE_CONFIG_REF_REQUIRED"),
-      source_config_hash: requireStringV1(input.soil_hydraulic_source_config_hash, "SOIL_HYDRAULIC_SOURCE_CONFIG_HASH_REQUIRED"),
+      source_config_ref: soilHydraulicSourceConfigRef,
+      source_config_hash: soilHydraulicSourceConfigHash,
       root_zone_depth_mm: MCFT_CAP_02_CONTINUATION_CROP_ROOT_ZONE_DEPTH_MM_V1,
       wilting_point_fraction: MCFT_CAP_02_CONTINUATION_WILTING_POINT_FRACTION_V1,
       wilting_point_storage_mm: MCFT_CAP_02_CONTINUATION_WILTING_POINT_STORAGE_MM_V1,
@@ -120,7 +146,7 @@ function freezePayloadV1(input: CompileContinuationRuntimeConfigInputV1): Contin
     irrigation_input_policy: {
       policy_id: MCFT_CAP_02_CONTINUATION_IRRIGATION_INPUT_POLICY_V1,
       event_order: "executed_at_asc_ingested_at_asc_source_record_id_asc",
-      spatial_overlap_deduplication: "NOT_ESTABLISHED",
+      spatial_overlap_deduplication: MCFT_CAP_02_CONTINUATION_SPATIAL_OVERLAP_POLICY_V1,
     },
     no_observation_update_policy: {
       policy_id: MCFT_CAP_02_CONTINUATION_NO_OBSERVATION_POLICY_V1,
@@ -134,8 +160,8 @@ function freezePayloadV1(input: CompileContinuationRuntimeConfigInputV1): Contin
       computation_storage_variance_scale: MCFT_CAP_02_CONTINUATION_COMPUTATION_STORAGE_VARIANCE_SCALE_V1,
       rule: MCFT_CAP_02_CONTINUATION_ROUNDING_RULE_V1,
     },
-    soil_root_zone_config_refs: requireArrayV1([input.soil_root_zone_config_ref], "SOIL_ROOT_ZONE_CONFIG_REFS_REQUIRED"),
-    model_component_refs: requireArrayV1(input.model_component_refs, "MODEL_COMPONENT_REFS_REQUIRED"),
+    soil_root_zone_config_refs: [soilRootZoneConfigRef],
+    model_component_refs: modelComponentRefs,
   };
 }
 
