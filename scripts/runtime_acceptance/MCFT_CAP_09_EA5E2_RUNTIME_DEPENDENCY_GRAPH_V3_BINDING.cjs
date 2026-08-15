320aa4e47376eb8e977ffdf13b99ce9532ff1d20
liyongshang44-max <liyongshang44@gmail.com>
2026-08-15 08:18:51 -0700

    MCFT-CAP-09: add V3 dependency graph binding carrier

---
+++ scripts/runtime_acceptance/MCFT_CAP_09_EA5E2_RUNTIME_DEPENDENCY_GRAPH_V3_BINDING.cjs
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+"use strict";
+// EA5E2_RUNTIME_DEPENDENCY_GRAPH_SHA256=__EA5E2_RUNTIME_DEPENDENCY_GRAPH_SHA256__
+module.exports = Object.freeze({
+  schema_version: "geox_mcft_cap09_ea5e2_runtime_dependency_graph_binding_v3",
+  authority_effect: false,
+});
