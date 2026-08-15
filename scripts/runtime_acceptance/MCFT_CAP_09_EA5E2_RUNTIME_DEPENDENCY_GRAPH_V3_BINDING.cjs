a4f6503c6cb5c461be2959ef50bf8cef16a019be
liyongshang44-max <liyongshang44@gmail.com>
2026-08-15 08:27:06 -0700

    MCFT-CAP-09: bind T3R1 EA5E2 dependency graph V3

--- scripts/runtime_acceptance/MCFT_CAP_09_EA5E2_RUNTIME_DEPENDENCY_GRAPH_V3_BINDING.cjs
+++ scripts/runtime_acceptance/MCFT_CAP_09_EA5E2_RUNTIME_DEPENDENCY_GRAPH_V3_BINDING.cjs
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 "use strict";
-// EA5E2_RUNTIME_DEPENDENCY_GRAPH_SHA256=__EA5E2_RUNTIME_DEPENDENCY_GRAPH_SHA256__
+// EA5E2_RUNTIME_DEPENDENCY_GRAPH_SHA256=sha256:97752ed8bdf95db78f640413471bae6e670d8a7ac692f46c87f43e47ae5acdf5
 module.exports = Object.freeze({
   schema_version: "geox_mcft_cap09_ea5e2_runtime_dependency_graph_binding_v3",
   authority_effect: false,
