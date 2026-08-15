c22cec2aadd44956eb837f824a27c22019f92975
liyongshang44-max <liyongshang44@gmail.com>
2026-08-15 08:52:35 -0700

    MCFT-CAP-09: bind long-horizon EA5E2 dependency graph

--- scripts/runtime_acceptance/MCFT_CAP_09_EA5E2_RUNTIME_DEPENDENCY_GRAPH_V3_BINDING.cjs
+++ scripts/runtime_acceptance/MCFT_CAP_09_EA5E2_RUNTIME_DEPENDENCY_GRAPH_V3_BINDING.cjs
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 "use strict";
-// EA5E2_RUNTIME_DEPENDENCY_GRAPH_SHA256=sha256:97752ed8bdf95db78f640413471bae6e670d8a7ac692f46c87f43e47ae5acdf5
+// EA5E2_RUNTIME_DEPENDENCY_GRAPH_SHA256=sha256:2d0f77b7fed99bacfdcd5ff03d5f7e3fa861b60f2f5ff0f4b6c2223795fed534
 module.exports = Object.freeze({
   schema_version: "geox_mcft_cap09_ea5e2_runtime_dependency_graph_binding_v3",
   authority_effect: false,
