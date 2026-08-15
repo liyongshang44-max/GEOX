f32dd250eb9b722477c1f218888752f499fd583d
liyongshang44-max <liyongshang44@gmail.com>
2026-08-15 10:51:29 -0700

    MCFT-CAP-09: add rolling activation dependency graph V4 carrier

---
+++ scripts/runtime_acceptance/MCFT_CAP_09_EA5E2_RUNTIME_DEPENDENCY_GRAPH_V4_BINDING.cjs
@@ -0,0 +1,8 @@
+#!/usr/bin/env node
+"use strict";
+// EA5E2_ROLLING_RUNTIME_DEPENDENCY_GRAPH_SHA256=__EA5E2_ROLLING_RUNTIME_DEPENDENCY_GRAPH_SHA256__
+module.exports = Object.freeze({
+  schema_version: "geox_mcft_cap09_ea5e2_rolling_runtime_dependency_graph_binding_v4",
+  final_activation_orchestration: "ROLLING_PREBOUNDARY_BATCH_INTERSECTION",
+  authority_effect: false,
+});
