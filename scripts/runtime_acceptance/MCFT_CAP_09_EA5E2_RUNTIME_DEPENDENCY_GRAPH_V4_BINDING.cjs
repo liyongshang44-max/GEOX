b00733bbcc566e4422b6bd65e50252000f2b00ca
liyongshang44-max <liyongshang44@gmail.com>
2026-08-15 20:23:58 -0700

    MCFT-CAP-09: isolate rolling preboundary runner blobs

--- scripts/runtime_acceptance/MCFT_CAP_09_EA5E2_RUNTIME_DEPENDENCY_GRAPH_V4_BINDING.cjs
+++ scripts/runtime_acceptance/MCFT_CAP_09_EA5E2_RUNTIME_DEPENDENCY_GRAPH_V4_BINDING.cjs
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 "use strict";
-// EA5E2_ROLLING_RUNTIME_DEPENDENCY_GRAPH_SHA256=sha256:6cf80f71d530863af0095b2d34a72268c591a4be5ed85c1a254391c2bcdf4eea
+// EA5E2_ROLLING_RUNTIME_DEPENDENCY_GRAPH_SHA256=sha256:2e0376d0d4072a0d157ab1f34877ce729999754a2f9a365151f0f8c7d46be1c7
 module.exports = Object.freeze({
   schema_version: "geox_mcft_cap09_ea5e2_rolling_runtime_dependency_graph_binding_v4",
   final_activation_orchestration: "ROLLING_PREBOUNDARY_BATCH_INTERSECTION",
