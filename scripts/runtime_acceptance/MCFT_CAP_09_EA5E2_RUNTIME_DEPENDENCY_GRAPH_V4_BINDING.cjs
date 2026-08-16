98ac221028d25318da004c9f8f2c82e4a423b5a2
liyongshang44-max <liyongshang44@gmail.com>
2026-08-15 22:59:23 -0700

    MCFT-CAP-09: rebind rolling runtime dependency graph

--- scripts/runtime_acceptance/MCFT_CAP_09_EA5E2_RUNTIME_DEPENDENCY_GRAPH_V4_BINDING.cjs
+++ scripts/runtime_acceptance/MCFT_CAP_09_EA5E2_RUNTIME_DEPENDENCY_GRAPH_V4_BINDING.cjs
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 "use strict";
-// EA5E2_ROLLING_RUNTIME_DEPENDENCY_GRAPH_SHA256=sha256:03e3d118b2244e9e9e9c09eb13b292d24fbf590535bbd5be4a6a4f4563766a54
+// EA5E2_ROLLING_RUNTIME_DEPENDENCY_GRAPH_SHA256=sha256:a6088259500f0b6b5db9d42bea19e27e3595ac38c4812d62c86534d95229c8a2
 module.exports = Object.freeze({
   schema_version: "geox_mcft_cap09_ea5e2_rolling_runtime_dependency_graph_binding_v4",
   final_activation_orchestration: "ROLLING_PREBOUNDARY_BATCH_INTERSECTION",
