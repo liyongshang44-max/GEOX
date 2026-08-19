93f4f69bdf9ff12c86c97ad5d4e9b1a51bc927c5
liyongshang44-max <liyongshang44@gmail.com>
2026-08-18 20:13:03 -0700

    MCFT-CAP-09: rebind rolling dependency graph after soil retry

--- scripts/runtime_acceptance/MCFT_CAP_09_EA5E2_RUNTIME_DEPENDENCY_GRAPH_V4_BINDING.cjs
+++ scripts/runtime_acceptance/MCFT_CAP_09_EA5E2_RUNTIME_DEPENDENCY_GRAPH_V4_BINDING.cjs
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 "use strict";
-// EA5E2_ROLLING_RUNTIME_DEPENDENCY_GRAPH_SHA256=sha256:3e78a7948bdd74711b4193c910d81d06d7d05a0aa36b9f7f30f3bf23d9e308a7
+// EA5E2_ROLLING_RUNTIME_DEPENDENCY_GRAPH_SHA256=sha256:dae8be3dec4aef260d2b8eed24ab4c405fc184f920dafbee54b2f84a8c0655d2
 module.exports = Object.freeze({
   schema_version: "geox_mcft_cap09_ea5e2_rolling_runtime_dependency_graph_binding_v4",
   final_activation_orchestration: "ROLLING_PREBOUNDARY_BATCH_INTERSECTION",
