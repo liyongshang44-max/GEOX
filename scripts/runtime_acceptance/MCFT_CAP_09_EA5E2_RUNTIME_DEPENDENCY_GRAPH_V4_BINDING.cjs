35a9b0172123a45b56523aa5cf180eac8cd10215
liyongshang44-max <liyongshang44@gmail.com>
2026-08-21 08:47:53 -0700

    fix(mcft-cap09): rebind dependency graph after trigger retirement

--- scripts/runtime_acceptance/MCFT_CAP_09_EA5E2_RUNTIME_DEPENDENCY_GRAPH_V4_BINDING.cjs
+++ scripts/runtime_acceptance/MCFT_CAP_09_EA5E2_RUNTIME_DEPENDENCY_GRAPH_V4_BINDING.cjs
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 "use strict";
-// EA5E2_ROLLING_RUNTIME_DEPENDENCY_GRAPH_SHA256=sha256:25553662303bc018d4fd061f017d6912f6df98dba98999adcf6bdacbeb402044
+// EA5E2_ROLLING_RUNTIME_DEPENDENCY_GRAPH_SHA256=sha256:052b9c748667c6c0b6bc385d4a17b11665a52c7b8775cffd5f15b73f9ddbc13e
 module.exports = Object.freeze({
   schema_version: "geox_mcft_cap09_ea5e2_rolling_runtime_dependency_graph_binding_v4",
   final_activation_orchestration: "ROLLING_PREBOUNDARY_BATCH_INTERSECTION",
