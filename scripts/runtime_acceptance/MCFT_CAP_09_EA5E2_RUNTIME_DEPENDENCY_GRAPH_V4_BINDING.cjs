3fa0121015098931dcd30f73691623d2a7e8d86f
liyongshang44-max <liyongshang44@gmail.com>
2026-08-25 10:35:40 -0700

    fix(mcft-cap09): rebind EA5E2 runtime graph after ingress revalidation

--- scripts/runtime_acceptance/MCFT_CAP_09_EA5E2_RUNTIME_DEPENDENCY_GRAPH_V4_BINDING.cjs
+++ scripts/runtime_acceptance/MCFT_CAP_09_EA5E2_RUNTIME_DEPENDENCY_GRAPH_V4_BINDING.cjs
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 "use strict";
-// EA5E2_ROLLING_RUNTIME_DEPENDENCY_GRAPH_SHA256=sha256:052b9c748667c6c0b6bc385d4a17b11665a52c7b8775cffd5f15b73f9ddbc13e
+// EA5E2_ROLLING_RUNTIME_DEPENDENCY_GRAPH_SHA256=sha256:b222cc98c5f518a42021dfea958215f4a9d8615463de02108af29c4d23987218
 module.exports = Object.freeze({
   schema_version: "geox_mcft_cap09_ea5e2_rolling_runtime_dependency_graph_binding_v4",
   final_activation_orchestration: "ROLLING_PREBOUNDARY_BATCH_INTERSECTION",
