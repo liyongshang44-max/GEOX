01afd2cb095fcabb72b7f2aafbe995023ffb055e
liyongshang44-max <liyongshang44@gmail.com>
2026-08-26 06:11:10 -0700

    chore(mcft-cap09): rebind EA5E2 graph for phase2 provider extraction

--- scripts/runtime_acceptance/MCFT_CAP_09_EA5E2_RUNTIME_DEPENDENCY_GRAPH_V4_BINDING.cjs
+++ scripts/runtime_acceptance/MCFT_CAP_09_EA5E2_RUNTIME_DEPENDENCY_GRAPH_V4_BINDING.cjs
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 "use strict";
-// EA5E2_ROLLING_RUNTIME_DEPENDENCY_GRAPH_SHA256=sha256:018c5d02cb5c2118e4dea329d74c083872117beb58c5e22c4a190b7a63f8358c
+// EA5E2_ROLLING_RUNTIME_DEPENDENCY_GRAPH_SHA256=sha256:d36723bf097085bee214d100b192ff719f124d3cb81bf2684d4c6f968c6eee7c
 module.exports = Object.freeze({
   schema_version: "geox_mcft_cap09_ea5e2_rolling_runtime_dependency_graph_binding_v4",
   final_activation_orchestration: "ROLLING_PREBOUNDARY_BATCH_INTERSECTION",
