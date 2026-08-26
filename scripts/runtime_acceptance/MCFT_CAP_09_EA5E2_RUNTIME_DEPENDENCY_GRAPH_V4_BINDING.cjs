f6b20594f100f7fdf8d6b549fa926e54c3dee98c
liyongshang44-max <liyongshang44@gmail.com>
2026-08-26 13:04:46 -0700

    gov(mcft-cap09): rebind graph to GFS acquisition provider

--- scripts/runtime_acceptance/MCFT_CAP_09_EA5E2_RUNTIME_DEPENDENCY_GRAPH_V4_BINDING.cjs
+++ scripts/runtime_acceptance/MCFT_CAP_09_EA5E2_RUNTIME_DEPENDENCY_GRAPH_V4_BINDING.cjs
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 "use strict";
-// EA5E2_ROLLING_RUNTIME_DEPENDENCY_GRAPH_SHA256=sha256:216383cea2288336f88f841464ea0b5e2fb11d0586a85bb4bd35b0bfac74d1f7
+// EA5E2_ROLLING_RUNTIME_DEPENDENCY_GRAPH_SHA256=sha256:b4355cd78b750db119d0b56560a6a51436a1fd2c3eb6ef92e82df6c8de90e873
 module.exports = Object.freeze({
   schema_version: "geox_mcft_cap09_ea5e2_rolling_runtime_dependency_graph_binding_v4",
   final_activation_orchestration: "ROLLING_PREBOUNDARY_BATCH_INTERSECTION",
