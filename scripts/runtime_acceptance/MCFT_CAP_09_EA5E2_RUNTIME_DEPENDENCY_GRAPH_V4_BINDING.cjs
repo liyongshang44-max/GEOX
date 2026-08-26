6d9711c4b55c3689299c2312baa0741b317138ab
liyongshang44-max <liyongshang44@gmail.com>
2026-08-25 19:15:27 -0700

    fix(mcft-cap09): rebind EA5E2 graph after CP5 routing

--- scripts/runtime_acceptance/MCFT_CAP_09_EA5E2_RUNTIME_DEPENDENCY_GRAPH_V4_BINDING.cjs
+++ scripts/runtime_acceptance/MCFT_CAP_09_EA5E2_RUNTIME_DEPENDENCY_GRAPH_V4_BINDING.cjs
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 "use strict";
-// EA5E2_ROLLING_RUNTIME_DEPENDENCY_GRAPH_SHA256=sha256:b222cc98c5f518a42021dfea958215f4a9d8615463de02108af29c4d23987218
+// EA5E2_ROLLING_RUNTIME_DEPENDENCY_GRAPH_SHA256=sha256:e61c9f6298dba7dece25f6ec1c4968de729285314ea7c450e13edf8514b5672e
 module.exports = Object.freeze({
   schema_version: "geox_mcft_cap09_ea5e2_rolling_runtime_dependency_graph_binding_v4",
   final_activation_orchestration: "ROLLING_PREBOUNDARY_BATCH_INTERSECTION",
