a08044d30b104270c93003050e196490e83d7e07
liyongshang44-max <liyongshang44@gmail.com>
2026-08-26 11:11:24 -0700

    fix(mcft-cap09): rebind EA5E2 graph digest to KBS modules

--- scripts/runtime_acceptance/MCFT_CAP_09_EA5E2_RUNTIME_DEPENDENCY_GRAPH_V4_BINDING.cjs
+++ scripts/runtime_acceptance/MCFT_CAP_09_EA5E2_RUNTIME_DEPENDENCY_GRAPH_V4_BINDING.cjs
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 "use strict";
-// EA5E2_ROLLING_RUNTIME_DEPENDENCY_GRAPH_SHA256=sha256:c26ba1f8493626975051b49919f958be90f01d13d8b33411358a331b81899d95
+// EA5E2_ROLLING_RUNTIME_DEPENDENCY_GRAPH_SHA256=sha256:fb2da6a4d7b6a07f33561f49ebbb52226fa9bf5559a5e415d2dbf8459629c891
 module.exports = Object.freeze({
   schema_version: "geox_mcft_cap09_ea5e2_rolling_runtime_dependency_graph_binding_v4",
   final_activation_orchestration: "ROLLING_PREBOUNDARY_BATCH_INTERSECTION",
