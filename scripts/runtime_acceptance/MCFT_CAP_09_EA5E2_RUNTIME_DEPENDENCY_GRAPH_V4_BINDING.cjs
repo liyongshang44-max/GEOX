2e38a5b853bd2bb62233d2192e7eb0fce7b99733
liyongshang44-max <liyongshang44@gmail.com>
2026-08-26 11:45:57 -0700

    gov(mcft-cap09): rebind EA5E2 graph for product GFS core

--- scripts/runtime_acceptance/MCFT_CAP_09_EA5E2_RUNTIME_DEPENDENCY_GRAPH_V4_BINDING.cjs
+++ scripts/runtime_acceptance/MCFT_CAP_09_EA5E2_RUNTIME_DEPENDENCY_GRAPH_V4_BINDING.cjs
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 "use strict";
-// EA5E2_ROLLING_RUNTIME_DEPENDENCY_GRAPH_SHA256=sha256:fb2da6a4d7b6a07f33561f49ebbb52226fa9bf5559a5e415d2dbf8459629c891
+// EA5E2_ROLLING_RUNTIME_DEPENDENCY_GRAPH_SHA256=sha256:ee954aa7efe3e845be86f8feb9523d98a37e1a946bb32da3655f13dcd73f13d3
 module.exports = Object.freeze({
   schema_version: "geox_mcft_cap09_ea5e2_rolling_runtime_dependency_graph_binding_v4",
   final_activation_orchestration: "ROLLING_PREBOUNDARY_BATCH_INTERSECTION",
