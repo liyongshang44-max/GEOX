7d59abff2c1fe3df9e73f11316346d3940c6c161
liyongshang44-max <liyongshang44@gmail.com>
2026-08-26 06:42:42 -0700

    chore(mcft-cap09): finalize EA5E2 phase2 dependency graph binding

--- scripts/runtime_acceptance/MCFT_CAP_09_EA5E2_RUNTIME_DEPENDENCY_GRAPH_V4_BINDING.cjs
+++ scripts/runtime_acceptance/MCFT_CAP_09_EA5E2_RUNTIME_DEPENDENCY_GRAPH_V4_BINDING.cjs
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 "use strict";
-// EA5E2_ROLLING_RUNTIME_DEPENDENCY_GRAPH_SHA256=sha256:d36723bf097085bee214d100b192ff719f124d3cb81bf2684d4c6f968c6eee7c
+// EA5E2_ROLLING_RUNTIME_DEPENDENCY_GRAPH_SHA256=sha256:c26ba1f8493626975051b49919f958be90f01d13d8b33411358a331b81899d95
 module.exports = Object.freeze({
   schema_version: "geox_mcft_cap09_ea5e2_rolling_runtime_dependency_graph_binding_v4",
   final_activation_orchestration: "ROLLING_PREBOUNDARY_BATCH_INTERSECTION",
