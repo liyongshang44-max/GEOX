c6c400c94b748a14ed4d01c5d3020ced5d419c90
liyongshang44-max <liyongshang44@gmail.com>
2026-08-26 21:29:06 -0700

    gov(mcft-cap09): rebind Phase3 provider-maintenance graph

--- scripts/runtime_acceptance/MCFT_CAP_09_EA5E2_RUNTIME_DEPENDENCY_GRAPH_V4_BINDING.cjs
+++ scripts/runtime_acceptance/MCFT_CAP_09_EA5E2_RUNTIME_DEPENDENCY_GRAPH_V4_BINDING.cjs
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 "use strict";
-// EA5E2_ROLLING_RUNTIME_DEPENDENCY_GRAPH_SHA256=sha256:9d873d06767fe2db3a1ccf299ebd7e6a2a184fad85bfdfb8d4e938cd5141e311
+// EA5E2_ROLLING_RUNTIME_DEPENDENCY_GRAPH_SHA256=sha256:c2681bbc65a05e9b9233d0f37ca7f5582cade8012713c66e4c66daba82346da1
 module.exports = Object.freeze({
   schema_version: "geox_mcft_cap09_ea5e2_rolling_runtime_dependency_graph_binding_v4",
   final_activation_orchestration: "ROLLING_PREBOUNDARY_BATCH_INTERSECTION",
