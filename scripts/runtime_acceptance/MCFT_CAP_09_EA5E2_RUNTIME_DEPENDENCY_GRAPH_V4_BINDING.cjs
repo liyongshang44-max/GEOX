de9b00b3ed808004b5457317cbc476a92faaedd1
liyongshang44-max <liyongshang44@gmail.com>
2026-08-26 20:50:47 -0700

    gov(mcft-cap09): rebind Phase3 exact dependency graph

--- scripts/runtime_acceptance/MCFT_CAP_09_EA5E2_RUNTIME_DEPENDENCY_GRAPH_V4_BINDING.cjs
+++ scripts/runtime_acceptance/MCFT_CAP_09_EA5E2_RUNTIME_DEPENDENCY_GRAPH_V4_BINDING.cjs
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 "use strict";
-// EA5E2_ROLLING_RUNTIME_DEPENDENCY_GRAPH_SHA256=sha256:3a049cb7cc14cd7f0ef702f6021422c68a8565595b47e8132da9847281d68304
+// EA5E2_ROLLING_RUNTIME_DEPENDENCY_GRAPH_SHA256=sha256:9d873d06767fe2db3a1ccf299ebd7e6a2a184fad85bfdfb8d4e938cd5141e311
 module.exports = Object.freeze({
   schema_version: "geox_mcft_cap09_ea5e2_rolling_runtime_dependency_graph_binding_v4",
   final_activation_orchestration: "ROLLING_PREBOUNDARY_BATCH_INTERSECTION",
