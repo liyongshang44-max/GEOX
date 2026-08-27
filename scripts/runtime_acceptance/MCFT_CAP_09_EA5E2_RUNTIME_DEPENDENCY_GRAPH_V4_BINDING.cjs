3513c422e9c759919c477adf3b05905ba95f9846
liyongshang44-max <liyongshang44@gmail.com>
2026-08-26 20:12:56 -0700

    gov(mcft-cap09): rebind graph to product GFS bundle decoder

--- scripts/runtime_acceptance/MCFT_CAP_09_EA5E2_RUNTIME_DEPENDENCY_GRAPH_V4_BINDING.cjs
+++ scripts/runtime_acceptance/MCFT_CAP_09_EA5E2_RUNTIME_DEPENDENCY_GRAPH_V4_BINDING.cjs
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 "use strict";
-// EA5E2_ROLLING_RUNTIME_DEPENDENCY_GRAPH_SHA256=sha256:05c91c31eb0c400dde468d34e06666e8be30dd30e71ed5b63235978eb4c5bd8d
+// EA5E2_ROLLING_RUNTIME_DEPENDENCY_GRAPH_SHA256=sha256:3a049cb7cc14cd7f0ef702f6021422c68a8565595b47e8132da9847281d68304
 module.exports = Object.freeze({
   schema_version: "geox_mcft_cap09_ea5e2_rolling_runtime_dependency_graph_binding_v4",
   final_activation_orchestration: "ROLLING_PREBOUNDARY_BATCH_INTERSECTION",
