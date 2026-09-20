7e6ab37a36751ae0c3b92ace4d85cb2f2fac4086
liyongshang44-max <liyongshang44@gmail.com>
2026-09-20 16:20:21 +0800

    test(mcft-cap09): bind exact EA5E2 generated dependency graph

--- scripts/runtime_acceptance/MCFT_CAP_09_EA5E2_RUNTIME_DEPENDENCY_GRAPH_V4_BINDING.cjs
+++ scripts/runtime_acceptance/MCFT_CAP_09_EA5E2_RUNTIME_DEPENDENCY_GRAPH_V4_BINDING.cjs
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 "use strict";
-// EA5E2_ROLLING_RUNTIME_DEPENDENCY_GRAPH_SHA256=sha256:f7158df2faae3223a258c7220e63b9ad2b690cfa55c4e0cebb1a59f24212d624
+// EA5E2_ROLLING_RUNTIME_DEPENDENCY_GRAPH_SHA256=sha256:c3685c7e376637f8f3068a97f47265e00b7e111ad2709df0e2f1792fd103d2fb
 module.exports = Object.freeze({
   schema_version: "geox_mcft_cap09_ea5e2_rolling_runtime_dependency_graph_binding_v4",
   final_activation_orchestration: "ROLLING_PREBOUNDARY_BATCH_INTERSECTION",
